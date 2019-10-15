4924e23572cfddbef7d8a515301497dda16ffc7a
Omar Jbara <omar.jbara2@gmail.com>
2019-10-15 22:33:37 +0200

    - Added Heroku trusted proxies

--- public/index.php
+++ public/index.php
@@ -12,8 +12,13 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
+if($_SERVER['APP_ENV'] === 'prod') {
+    $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+}
+if($trustedProxies) {
+    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
 }
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
