62411d8258f511a6899a825f724d83c9c53bd6bb
Omar Jbara <omar.jbara2@gmail.com>
2019-10-15 19:38:53 +0200

    - Send Tweet when the download is ready

--- src/MessageHandler/Command/ProcessTwitterVideoHandler.php
+++ src/MessageHandler/Command/ProcessTwitterVideoHandler.php
@@ -9,8 +9,10 @@ use App\Downloader\TwitterVideoDownloader;
 use App\Message\Command\ProcessTwitterVideo;
 use App\Model\Tweet;
 use App\Repository\VideoRequestRepository;
+use DG\Twitter\Twitter;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
 class ProcessTwitterVideoHandler implements MessageHandlerInterface
@@ -20,9 +22,13 @@ class ProcessTwitterVideoHandler implements MessageHandlerInterface
     private $client;
     private $repository;
     private $em;
+    private $twitterClient;
+    private $router;
 
     public function __construct(
         TwitterClient $client,
+        Twitter $twitterClient,
+        RouterInterface $router,
         TwitterVideoDownloader $downloader,
         VideoRequestRepository $repository,
         EntityManagerInterface $em
@@ -31,6 +37,8 @@ class ProcessTwitterVideoHandler implements MessageHandlerInterface
         $this->client = $client;
         $this->repository = $repository;
         $this->em = $em;
+        $this->twitterClient = $twitterClient;
+        $this->router = $router;
     }
 
     public function __invoke(ProcessTwitterVideo $message)
@@ -54,12 +62,26 @@ class ProcessTwitterVideoHandler implements MessageHandlerInterface
                     ->setProcessed(true)
                     ->setMimeType($video->getMimeType())
                     ->setFilename($filename);
+
+                $this->twitterClient->send(sprintf(
+                    'tiens chef ton lien de dl est prêt : %s',
+                    $this->router->generate('video_request.download', [
+                        'id' => $request->getId()
+                    ], RouterInterface::ABSOLUTE_URL)
+                ),
+                    null,
+                    [
+                        'in_reply_to_status_id' => Tweet::extractIdFromUrl($request->getReplyUrl()),
+                        'auto_populate_reply_metadata' => true
+                    ]
+                );
+
+                $this->em->flush();
             } else {
                 // The requested tweet did not have any videos attached, so we can delete the request
                 $this->em->remove($request);
+                $this->em->flush();
             }
-
-            $this->em->flush();
         } else {
             throw new NotFoundResourceException(
                 sprintf('Could not find VideoRequest with ID "%s"', $message->getRequestId())
